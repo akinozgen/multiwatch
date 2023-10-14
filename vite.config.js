6d7f96ea8ed8f5403dee3026370a6b459e9ca2f1
Akın Özgen <akinozgen17@outlook.com>
2023-10-14 20:30:32 +0300

    netlify redirection

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
 
+// copy __redirects file to dist folder
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
